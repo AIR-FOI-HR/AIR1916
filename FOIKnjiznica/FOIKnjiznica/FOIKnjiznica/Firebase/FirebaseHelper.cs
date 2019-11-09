6e1037b31b6537b0ac55caa1ec43f8dd683e11e5
Matej Lipovača <clicker34@gmail.com>
2019-11-09 13:38:09 +0100

    + Dodan Firebase Real-Time Database
    Firebase Real-Time baza koja služi za dohvaćanje/zapisivanje podataka u realnom vremenu w/ Xamarin Forms.

---
+++ FOIKnjiznica/FOIKnjiznica/FOIKnjiznica/Firebase/FirebaseHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Firebase.Database;
+using Firebase.Database.Query;
+
+namespace FOIKnjiznica.Firebase
+{
+    public class FirebaseHelper
+    {
+        FirebaseClient database = new FirebaseClient("https://foiknjiznica.firebaseio.com/");
+    }
+}
