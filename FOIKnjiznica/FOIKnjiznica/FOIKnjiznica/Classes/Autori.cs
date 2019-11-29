ddec03d94363c94506c9f8b3e47cb50f2b15641c
sdrvoderi <stiven.drvoderic98@gmail.com>
2019-11-29 16:58:56 +0100

    Izrada Controllera za dohvaćanje Autora te izrada klase Autori
    Izrađen je controller za dohvaćanje autora sa baze podataka te je kreirana klasa autori. Prethodno navedene stvari su potrebna kako bi se kasnije realizirao filtar prema autorima.

---
+++ FOIKnjiznica/FOIKnjiznica/FOIKnjiznica/Classes/Autori.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FOIKnjiznica.Classes
+{
+    class Autori
+    {
+        public int id { get; set; }
+
+        public string ime { get; set; }
+
+        public string prezime { get; set; }
+    }
+}
