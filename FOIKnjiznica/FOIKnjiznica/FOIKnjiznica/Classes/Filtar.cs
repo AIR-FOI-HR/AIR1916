79d492484f134307c3318a490b2e7c60570b882b
sdrvoderi <stiven.drvoderic98@gmail.com>
2019-12-03 15:42:39 +0100

    Kreiranje statične metode za filtriranje liste publikacija
    Kreirana je statična metoda unutar klase Filtar koja kao parametar prima listu publikacija te vraća filtriranu listu publikacija.  Metoda koristi liste iz pripadajuće statične klase koji predstavljaju liste filtera te se lista publikacija filtrira po svakoj listi i time se dobije konačno filtrirana lista publikacija. Ako je lista nekog filtra prazna dalje se proslijeđuje cijela lista publikacija jer onda filtar nije aktivan. Na taj način ako se filtar ne koristi, to jest ako su sve liste prazne, neće utjecati na rad programa.

--- FOIKnjiznica/FOIKnjiznica/FOIKnjiznica/Classes/Filtar.cs
+++ FOIKnjiznica/FOIKnjiznica/FOIKnjiznica/Classes/Filtar.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace FOIKnjiznica.Classes
@@ -10,5 +11,89 @@ namespace FOIKnjiznica.Classes
         public static List<Izdavaci> filtarIzdavaci { get; set; } = new List<Izdavaci>();
         public static List<Kategorije> filtarKategorije { get; set; } = new List<Kategorije>();
         public static List<Slova> filtarSlova { get; set; } = new List<Slova>();
+
+        public static List<Publikacije> FiltrirajPublikacije(List<Publikacije> trenutnaListaPublikacija)
+        {
+            List<Publikacije> filtriranaListaPublikacija = new List<Publikacije>();
+            List<Publikacije> privremenaFiltriranaListaPublikacija = new List<Publikacije>();
+            bool prolaziFiltar = false;
+
+            //Filtriranje po id autora ako filtar autora sadrži barem jedan element, ako ne filtriranaLista sadrži sve publikacije
+
+            if (filtarAutori.Count > 0)
+            {
+                    foreach (Publikacije trenutnaPublikacija in trenutnaListaPublikacija)
+                    {
+                        prolaziFiltar = filtarAutori.Any(item => item.id == trenutnaPublikacija.id);
+
+                        if(prolaziFiltar == true)
+                        {
+                            filtriranaListaPublikacija.Add(trenutnaPublikacija);
+                            prolaziFiltar = false;
+                        }
+                    }
+            }
+            else
+            {
+                filtriranaListaPublikacija = trenutnaListaPublikacija;
+            }
+
+            //Filtriranje po id izdavaca ako filtar izdavaca sadrži barem jedan element, ako ne filtriranaLista sadrži sve publikacije
+            if (filtarIzdavaci.Count > 0)
+            {
+                foreach (Publikacije trenutnaPublikacija in filtriranaListaPublikacija)
+                {
+                    prolaziFiltar = filtarIzdavaci.Any(item => item.id == trenutnaPublikacija.id);
+
+                    if (prolaziFiltar == true)
+                    {
+                        privremenaFiltriranaListaPublikacija.Add(trenutnaPublikacija);
+                        prolaziFiltar = false;
+                    }
+                }
+
+                trenutnaListaPublikacija = privremenaFiltriranaListaPublikacija;
+                privremenaFiltriranaListaPublikacija.Clear();
+            }
+
+            //Filtriranje po id kategorije ako filtar kategorija sadrži barem jedan element, ako ne filtriranaLista sadrži sve publikacije
+            if (filtarKategorije.Count > 0)
+            {
+                foreach (Publikacije trenutnaPublikacija in filtriranaListaPublikacija)
+                {
+                    prolaziFiltar = filtarKategorije.Any(item => item.id == trenutnaPublikacija.id);
+
+                    if (prolaziFiltar == true)
+                    {
+                        privremenaFiltriranaListaPublikacija.Add(trenutnaPublikacija);
+                        prolaziFiltar = false;
+                    }
+                }
+
+                trenutnaListaPublikacija = privremenaFiltriranaListaPublikacija;
+                privremenaFiltriranaListaPublikacija.Clear();
+            }
+
+            //Filtriranje po početnom slovu ako filtar slova sadrži barem jedan element, ako ne filtriranaLista sadrži sve publikacije
+            if (filtarSlova.Count > 0)
+            {
+                foreach (Publikacije trenutnaPublikacija in filtriranaListaPublikacija)
+                {
+                    prolaziFiltar = filtarSlova.Any(item => item.slovo == trenutnaPublikacija.naziv[0]);
+
+                    if (prolaziFiltar == true)
+                    {
+                        privremenaFiltriranaListaPublikacija.Add(trenutnaPublikacija);
+                        prolaziFiltar = false;
+                    }
+                }
+
+                trenutnaListaPublikacija = privremenaFiltriranaListaPublikacija;
+                privremenaFiltriranaListaPublikacija.Clear();
+            }
+
+            return filtriranaListaPublikacija;
+        }
+
     }
 }
