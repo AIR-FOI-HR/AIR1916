8b9e0f20188be3a606fce527c61d2471cbcd9ca8
Matej Lipovača <clicker34@gmail.com>
2019-12-06 18:32:20 +0100

    Imeplementiran splash screen za aplikaciju (tokom Load-anje aplikacije se ucitava TextView + .json file kao GIF)
    Imeplementiran splash screen za aplikaciju (tokom Load-anje aplikacije se ucitava TextView + .json file kao GIF)

--- FOIKnjiznica/FOIKnjiznica/FOIKnjiznica.Android/MainActivity.cs
+++ FOIKnjiznica/FOIKnjiznica/FOIKnjiznica.Android/MainActivity.cs
@@ -7,10 +7,11 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using FFImageLoading.Forms.Platform;
+using Lottie.Forms.Droid;
 
 namespace FOIKnjiznica.Droid
 {
-    [Activity(Label = "FOIKnjiznica", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "FOIKnjiznica", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -25,6 +26,8 @@ namespace FOIKnjiznica.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            //Inicijalizacija Android Lottie nugget paketa
+            AnimationViewRenderer.Init();
             CachedImageRenderer.Init(true);
             LoadApplication(new App());
         }
