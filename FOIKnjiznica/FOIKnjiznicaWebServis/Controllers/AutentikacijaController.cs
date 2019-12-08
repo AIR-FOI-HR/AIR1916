ca07d52ac7cbfa8d3ee5344615fe8165e63fa7a3
akudelic1 <akudelic1@users.noreply.github.com>
2019-12-08 15:26:56 +0100

    Dohvaćanje korisničkog imena korisnika nakon logina na login.foi.hr
    Nakon što se korisnik prijavi u sustav, automatski se redirecta na našu indexnu stranicu na localhostu. Kako bi dohvatili korisničko ime, zasada to radimo ručnim utipkavanjem /Autentikacija na kraj URL-a. Točnije, nakon redirecta na našu stranicu dodajemo "/Autentikacija" u URL-u nakon čega se ispisuje korisničko ime prijavljenog korisnika. Za sada nema dohvata svih ostalih podataka koji bi nam bili potrebni, poput imena i prezimena. #63

---
+++ FOIKnjiznica/FOIKnjiznicaWebServis/Controllers/AutentikacijaController.cs
@@ -0,0 +1,30 @@
+﻿using DotNetCasClient;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FOIKnjiznicaWebServis.Controllers
+{
+    public class AutentikacijaController : Controller
+    {
+        // 
+        //GET: /Autentikacija/
+
+        public string Index()
+        {
+            return HttpContext.User.Identity.Name;
+        }
+        
+
+        //
+        //GET: /Odjava/
+
+        public void Odjava()
+        {
+            CasAuthentication.SingleSignOut();
+        }
+    }
+}
