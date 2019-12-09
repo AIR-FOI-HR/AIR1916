423deedbd66c5454398dc6b7730ff76e7e344712
sdrvoderi <stiven.drvoderic98@gmail.com>
2019-12-09 18:54:27 +0100

    Kreirana statična klasa za pohranjivanje člana/korisnika
    Kreirana je statična klasa Clanovi za pohranjivanje podataka o članu te je kreirana lista koja sadržava publikacije koje su dodane kao favoriti tome članu

---
+++ FOIKnjiznica/FOIKnjiznica/FOIKnjiznica/Classes/Clanovi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FOIKnjiznica.Classes
+{
+    public static class Clanovi
+    {
+        public static int id { get; set; }
+        public static string hrEduPersonUniqueID { get; set; }
+        public static string ime { get; set; }
+        public static string prezime { get; set; }
+        public static string mobitelID { get; set; }
+        public static List<Publikacije> listaFavorita { get; set; }
+
+    }
+}
