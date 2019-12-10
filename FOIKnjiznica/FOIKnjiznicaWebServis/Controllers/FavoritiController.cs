60ab40420449ef9cdb8e0e43a946b9c609e14a39
sdrvoderi <stiven.drvoderic98@gmail.com>
2019-12-10 17:41:04 +0100

    Kreiranje kontrolera za dohvaćanje cijele tablice Je_Favorit te dohvaćanje favorita jednog člana
    Napravljen je kontroler sa dvije GET metode. Prva dohvaća cijelu tablicu favorita, a druga metoda prima parametar koji je id trenutnog člana te prema id-u uzima favorite tog člana.

---
+++ FOIKnjiznica/FOIKnjiznicaWebServis/Controllers/FavoritiController.cs
@@ -0,0 +1,70 @@
+﻿using FOIKnjiznicaWebServis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FOIKnjiznicaWebServis.Controllers
+{
+    public class FavoritiController : ApiController
+    {
+        KnjiznicaEntities db = new KnjiznicaEntities();
+
+        // GET api/Favoriti
+        public IEnumerable<Object> Get()
+        {
+            var upit = from Je_Favorit in db.Je_Favorit
+                       select new
+                       {
+                           idFavorita = Je_Favorit.PublikacijeId,
+                           idClana = Je_Favorit.ClanoviId
+                       };
+
+            return upit.ToList<Object>();
+        }
+
+        // GET api/Favoriti/5
+        public List<Object> Get(int id)
+        {
+            var upit = from Je_Favorit in db.Je_Favorit
+                       where id == Je_Favorit.ClanoviId 
+                       select new
+                       {
+                           idFavorita = Je_Favorit.PublikacijeId
+                       };
+
+            return upit.ToList<Object>();
+        }
+
+        public HttpResponseMessage DohvatiFavorite()
+        {
+            var favorit = db.Je_Favorit.FirstOrDefault();
+
+            if (favorit == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+        }
+
+        // POST api/Favoriti
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/Favoriti/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/Favoriti/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
