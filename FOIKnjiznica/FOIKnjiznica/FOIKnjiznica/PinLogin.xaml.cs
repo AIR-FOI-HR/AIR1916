dd008d6f289445309b0c48eb642b4db14dde5ba6
Matija <48254592+mivanic9@users.noreply.github.com>
2019-12-17 17:36:28 +0100

    Dodavanje klika na gumbove
    Dodavanje eventa na gumbove, kako bismo mogli proslijedi određeni glumb, kad se klikne #70

--- FOIKnjiznica/FOIKnjiznica/FOIKnjiznica/PinLogin.xaml.cs
+++ FOIKnjiznica/FOIKnjiznica/FOIKnjiznica/PinLogin.xaml.cs
@@ -16,5 +16,54 @@ namespace FOIKnjiznica
         {
             InitializeComponent();
         }
+
+        private void DodajBroj(string broj)
+        {
+
+        }
+        private void BtnJedan(object sender, EventArgs e)
+        {
+            DodajBroj("1");
+        }
+        private void BtnDva(object sender, EventArgs e)
+        {
+            DodajBroj("2");
+        }
+        private void BtnTri(object sender, EventArgs e)
+        {
+            DodajBroj("3");
+        }
+        private void BtnCetiri(object sender, EventArgs e)
+        {
+            DodajBroj("4");
+        }
+        private void BtnPet(object sender, EventArgs e)
+        {
+            DodajBroj("5");
+        }
+        private void BtnSest(object sender, EventArgs e)
+        {
+            DodajBroj("6");
+        }
+        private void BtnSedam(object sender, EventArgs e)
+        {
+            DodajBroj("7");
+        }
+        private void BtnOsam(object sender, EventArgs e)
+        {
+            DodajBroj("8");
+        }
+        private void BtnDevet(object sender, EventArgs e)
+        {
+            DodajBroj("9");
+        }
+        private void BtnNula(object sender, EventArgs e)
+        {
+            DodajBroj("0");
+        }
+        private void BtnDelete(object sender, EventArgs e)
+        {
+            //IzbrisiPrethodnuBrojku();
+        }
     }
 }
