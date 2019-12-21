df766136dabe9e7c1b1f587cc9f33d0c3085a74a
sdrvoderi <stiven.drvoderic98@gmail.com>
2019-12-21 17:28:53 +0100

    Klasa PovijestPublikacije
    Kreirana je klasa koja će se koristiti za spremanje podatka o rezervaciji/posudbi neke publikacije

---
+++ FOIKnjiznica/FOIKnjiznica/FOIKnjiznica/Classes/PovijestPublikacije.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FOIKnjiznica.Classes
+{
+    public class PovijestPublikacije
+    {
+        public int id { get; set; }
+        public int kopijaId { get; set; }
+        public int vrsta_statusaId { get; set; }
+        public int clanoviId { get; set; }
+
+        public DateTime datum { get; set; }
+        public DateTime datum_do { get; set; }
+        public string nazivPublikacije { get; set; }
+        public string nazivStatusa { get; set; }
+    }
+}
