a4c68b3fb363f5c39d5cf5df882112caacb2e7fb
sdrvoderi <stiven.drvoderic98@gmail.com>
2020-01-03 11:26:47 +0100

    Izmjena kontrolera i popup ekrana za rezerviranje
    Izmjenjen je kontroler za rezerviranje tako da se kreira novo stanje umjesto da se samo izmjenjuje postojeće stanje kopije. Izmjenjen je popup ekran tako da se u slučaju pokušaja rezerviranja kopije publikacije prikazuje datum trenutne rezervacije a ne svih stanja kopije.

--- FOIKnjiznica/FOIKnjiznicaWebServis/Controllers/GumbRezervirajController.cs
+++ FOIKnjiznica/FOIKnjiznicaWebServis/Controllers/GumbRezervirajController.cs
@@ -2,8 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace FOIKnjiznicaWebServis.Controllers
@@ -29,20 +27,28 @@ namespace FOIKnjiznicaWebServis.Controllers
         }
 
         // PUT: api/GumbRezerviraj/5
-        public void Put(int id)
+        public void Put([FromBody] PovijestPublikacije novoStanjePublikacije)
         {
-            var upit = db.Stanje_Publikacije.Where(s => s.KopijaId == id).SingleOrDefault();
+            //Dohvaćanje zadnjeg stanja kopije publikacije
+            var upit = db.Stanje_Publikacije.Where(s => s.KopijaId == novoStanjePublikacije.kopijaId && s.datum_do == db.Stanje_Publikacije.Where(c => c.KopijaId == novoStanjePublikacije.kopijaId).Max(d => d.datum_do)).SingleOrDefault(); 
 
             if (upit.Vrsta_StatusaId == 1)
             {
-                upit.Vrsta_StatusaId = 3;
-                upit.datum = DateTime.Now;
-                upit.datum_do = DateTime.Now.AddDays(5);
-                db.SaveChanges();
+                upit.datum_do = DateTime.Now;
+                db.Stanje_Publikacije.Add(new Stanje_Publikacije() 
+                { 
+                    datum = novoStanjePublikacije.datum, 
+                    datum_do = novoStanjePublikacije.datum_do, 
+                    KopijaId = novoStanjePublikacije.kopijaId, 
+                    Vrsta_StatusaId = novoStanjePublikacije.vrsta_statusaId,
+                    ClanoviId = novoStanjePublikacije.clanoviId 
+                });
+                db.SaveChanges();             
             }
+
             DateTime dt1 = DateTime.Parse(upit.datum_do.ToString());
             DateTime dt2 = DateTime.Now;
-            if (dt1 > dt2)
+            if (dt1 > dt2 && upit.Vrsta_StatusaId == 3)
             {
                 upit.Vrsta_StatusaId = 1;
             }
