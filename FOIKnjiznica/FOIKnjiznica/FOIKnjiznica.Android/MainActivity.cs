77984e6c153acb2e69c299441815db9ef6ea98ae
akudelic1 <akudelic1@users.noreply.github.com>
2020-01-04 10:50:51 +0100

    Prikaz FOI logina prilikom otvaranja aplikacije
    Pošto je došlo do problema sa Xamarin.Forms i webview mogućnostima, napravljen je WebFOIPrijava.axml (Android.Forms) koji se pokreće na OnCreate aplikacije te prikazuje webview bez problema. Također, za sad postoji error prilikom logina preko istog webviewa putem mobitela, ali na računalu radi bez problema (err_connection_refused). Kako bi projekt radio na localhostu preko mobitela dodan je i Conveyor by Keyoti. #19

--- FOIKnjiznica/FOIKnjiznica/FOIKnjiznica.Android/MainActivity.cs
+++ FOIKnjiznica/FOIKnjiznica/FOIKnjiznica.Android/MainActivity.cs
@@ -7,12 +7,16 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using FFImageLoading.Forms.Platform;
+using System.Net;
+using Android.Webkit;
+using Android.Net.Http;
 
 namespace FOIKnjiznica.Droid
 {
     [Activity(Label = "FOIKnjiznica", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        WebView webView;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -26,13 +30,41 @@ namespace FOIKnjiznica.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             CachedImageRenderer.Init(true);
-            LoadApplication(new App());
+            SetContentView(Resource.Layout.WebFOIPrijava);
+            if (true)
+            {
+                webView = FindViewById<WebView>(Resource.Id.prijavawebview);
+                webView.Settings.JavaScriptEnabled = true;
+                webView.Settings.JavaScriptCanOpenWindowsAutomatically = true;
+                webView.Settings.DomStorageEnabled = true;
+                webView.Settings.UseWideViewPort = true;
+                webView.Settings.LoadWithOverviewMode = true;
+
+                webView.SetWebViewClient(new HelloWebViewClient());
+                webView.LoadUrl("https://192.168.0.125:45455/");
+
+            }
+            //LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public class HelloWebViewClient : WebViewClient
+    {
+        public override bool ShouldOverrideUrlLoading(Android.Webkit.WebView view, IWebResourceRequest request)
+        {
+            view.LoadUrl(request.Url.ToString());
+            return false;
+        }
+
+        public override void OnReceivedSslError(Android.Webkit.WebView view, SslErrorHandler handler, SslError error)
+        {
+            handler.Proceed();
         }
     }
 }
