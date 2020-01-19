413c68262cb03ea591be233ad11deb1e2116dd5d
sdrvoderi <stiven.drvoderic98@gmail.com>
2020-01-19 21:39:11 +0100

    Dodavanje potrebnih datoteka te biblioteka
    Dodane su potrebne biblioteke za rad s AppCenter projektom te datoteka google-services. Testirano je slanje notifikacije.

--- FOIKnjiznica/FOIKnjiznica/FOIKnjiznica/MainMenuDetail.xaml.cs
+++ FOIKnjiznica/FOIKnjiznica/FOIKnjiznica/MainMenuDetail.xaml.cs
@@ -14,6 +14,9 @@ using System.Windows.Input;
 using Rg.Plugins.Popup.Services;
 using FOIKnjiznica.Classes;
 
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Push;
+
 namespace FOIKnjiznica
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -34,6 +37,8 @@ namespace FOIKnjiznica
             MessagingCenter.Subscribe<App>((App)Application.Current, "sortiranjePoAutoru", (sender) => { OsvjeziListuPublikacija(); });
             MessagingCenter.Subscribe<App>((App)Application.Current, "filtriranjePublikacija", (sender) => { OsvjeziListuPublikacija(); });
             MessagingCenter.Subscribe<App>((App)Application.Current, "resetiranjeFiltera", (sender) => { OsvjeziListuPublikacija(); });
+
+            AppCenter.Start("65068ce7-ba56-49b6-bc9e-03c39018c505", typeof(Push));
         }
 
         //Dohvacanje Publikacije za prikaz na zaslonu
