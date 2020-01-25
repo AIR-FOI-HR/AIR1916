7390cfd2f6995e287c49072bddf6d0e7764a79f0
Matija <48254592+mivanic9@users.noreply.github.com>
2020-01-26 00:49:54 +0100

    DodajPin kontolor
    Kreiran kontrola za potrebe dodavanja lozinke u db.
    #20  #22  #88

---
+++ FOIKnjiznica/FOIKnjiznicaWebServis/Controllers/DodajPinController.cs
@@ -0,0 +1,82 @@
+﻿using FOIKnjiznicaWebServis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FOIKnjiznicaWebServis.Controllers
+{
+    public class ClientAuth
+    {
+        public int ClanoviId { get; set; }
+        public int Auth_ProtocolId { get; set; }
+        public string podaci { get; set; }
+        public bool odabrano { get; set; }
+    }
+    public class DodajPinController : ApiController
+    {
+        foiknjiznicaEntities db = new foiknjiznicaEntities();
+        // GET: api/DodajPin
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/DodajPin/5
+        public ClientAuth Get(int id)
+        {
+            ClientAuth novi = new ClientAuth();
+            var upit = (from s in db.Clanovi_Auth_Protocol
+                        where s.ClanoviId == id
+                        select new
+                        {
+                            s.ClanoviId,
+                            s.Auth_ProtocolId,
+                            s.podaci,
+                            s.odabrano
+                        });
+
+            return novi;
+        }
+
+        // POST: api/DodajPin
+        public void Post([FromBody]ClientAuth clijentAuth)
+        {
+
+            using (var ctx = new foiknjiznicaEntities())
+            {
+                var upit = db.Clanovi_Auth_Protocol.Where(x => x.ClanoviId == clijentAuth.ClanoviId).SingleOrDefault();
+                if(upit != null)
+                {
+                    upit.Auth_ProtocolId = clijentAuth.Auth_ProtocolId;
+                    upit.podaci = clijentAuth.podaci;
+                    upit.odabrano = clijentAuth.odabrano;
+                    
+                }
+                else
+                {
+                    ctx.Clanovi_Auth_Protocol.Add(new Clanovi_Auth_Protocol()
+                    {
+                        ClanoviId = clijentAuth.ClanoviId,
+                        Auth_ProtocolId = clijentAuth.Auth_ProtocolId,
+                        podaci = clijentAuth.podaci,
+                        odabrano = clijentAuth.odabrano
+                    });
+                }
+                ctx.SaveChanges();
+            }
+        }
+
+        // PUT: api/DodajPin/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/DodajPin/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- FOIKnjiznica/FOIKnjiznicaWebServis/Models/knjiznicaEntities.Designer.cs
+++ FOIKnjiznica/FOIKnjiznicaWebServis/Models/knjiznicaEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Matej\Documents\GitHub\AIR1916\FOIKnjiznica\FOIKnjiznicaWebServis\Models\knjiznicaEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\FOI\Diplomski\AIR\Github\eKnjiznica\AIR1916\FOIKnjiznica\FOIKnjiznicaWebServis\Models\knjiznicaEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
