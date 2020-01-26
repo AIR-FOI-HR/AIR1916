f8959a2c807ebf3cc02a1208de0f2dd331ad0cd8
sdrvoderi <stiven.drvoderic98@gmail.com>
2020-01-26 19:01:06 +0100

    Dodano slanje poruke o rezervaciji te prekidu rezervacije
    #93
    Aplikacija kod rezerviranja šalje poruku tko je rezervirao, koju knjigu te do kada vrijedi rezervacija. Također šalje se poruka o prekidu.

--- FOIKnjiznica/FOIKnjiznica/FOIKnjiznica/PopUpPages/PrekidRezervacijePopupPage.xaml.cs
+++ FOIKnjiznica/FOIKnjiznica/FOIKnjiznica/PopUpPages/PrekidRezervacijePopupPage.xaml.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -40,7 +41,28 @@ namespace FOIKnjiznica.PopUpPages
 
             await httpClient.GetAsync("https://foiknjiznicaazurefunkcije.azurewebsites.net/api/OslobodenjeKnjigeNotifikacija?code=FE9IUHyiPZcyzcca7DEzTlBRVWh5HUElZIeLX4UE6JA73cPdoTyP9A==&idPublikacije=" + rezervacija.nazivPublikacije);
 
+            PosaljiObavijest(rezervacija.kopijaId);
+
             await PopupNavigation.Instance.PopAsync();
+        }
+
+        public void PosaljiObavijest(int idKopije)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+            mail.From = new MailAddress(Classes.Clanovi.hrEduPersonUniqueID);
+            mail.To.Add("sdrvoderi@foi.hr");
+            mail.Subject = "Prekid rezervacije";
+            mail.Body = $"Korisnik {Classes.Clanovi.hrEduPersonUniqueID} je upravo prekinuo rezervaciju knjige sa identifikacijskim brojem {idKopije.ToString()}";
+
+            SmtpServer.Port = 587;
+            SmtpServer.Host = "smtp.gmail.com";
+            SmtpServer.EnableSsl = true;
+            SmtpServer.UseDefaultCredentials = false;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("fknjiznica@gmail.com", "admin123!");
+
+            SmtpServer.Send(mail);
         }
     }
 }
